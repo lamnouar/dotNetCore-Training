5c4e4d770a4bdb7b2abf59992bdea6545ad2adca
abdelhak lamnouar <lamnouar.abdelhak@gmail.com>
2019-11-05 21:58:06 -0500

    Changing the project from .Net standard to .net core

---
+++ OdeToFood.DAL/IRestaurantData.cs
@@ -0,0 +1,63 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace OdeToFood.DAL
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetRestaurantById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private IList<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id = 1, Name="Luigi Pizza", Cuisine = CuisineType.Italian, Location="14 fleet street, London UK" },
+                new Restaurant{Id = 2, Name="Curry chicken", Cuisine = CuisineType.Indian, Location="1480 fleet street, London UK" }
+            };
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            restaurants.Add(newRestaurant);
+
+            return newRestaurant;
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant!=null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+    }
+}
