4b099836a42865fd12663ed2348e9791fce562f1
Abdelhak Lamnouar <lamnouar.abdelhak@gmail.com>
2019-11-28 09:56:23 -0500

    Update Program.cs

--- OdeToFood.Services/Program.cs
+++ OdeToFood.Services/Program.cs
@@ -19,6 +19,6 @@ namespace OdeToFood.Services
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
     }
 }
